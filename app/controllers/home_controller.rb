c476c5f6a05a070d53317f7b3d4daaac9e63dcef
Duncan Gough <duncan@suttree.com>
2016-11-25 20:15:56 +0000

    reverting to just one rating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @hrefs = Href.where(good: true).where('rating > -25 OR rating2 > 0').group('DATE(created_at)').order('created_at DESC').limit(12)
+    #@hrefs = Href.where(good: true).where('rating > -25 OR rating2 > 0').group('DATE(created_at)').order('created_at DESC').limit(12)
+    @hrefs = Href.where(good: true).where('rating > -25').group('DATE(created_at)').order('created_at DESC').limit(12)
   end
 end
